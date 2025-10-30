9f438f2b280a900b13a65796b5229d6ee9dc18e1
dongchandev <dc4653.dev@gmail.com>
2025-10-30 14:49:32 +0900

    init

---
+++ src/main/java/com/tdd/number/baseball/TddNumberBaseballApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.number.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddNumberBaseballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddNumberBaseballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdd/number/baseball/TddNumberBaseballApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tdd.number.baseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddNumberBaseballApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
